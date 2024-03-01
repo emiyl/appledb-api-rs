cc2e58edbec07a5b9b4ba35213b1cd15ff247404
emiyl <me@emiyl.com>
2024-03-01 15:39:50 +0000

    add main.json and index.json files

--- src/file.rs
+++ src/file.rs
@@ -1,15 +1,23 @@
 use std::fs;
-use std::io::Write;
+use std::io::{Result, Write};
 use std::path::Path;
 
 pub fn open_file_to_string(file_path: &str) -> String {
     fs::read_to_string(file_path).expect("Should have been able to read the file") as String
 }
 
-pub fn write_string_to_file(file_path: &str, contents: String) -> std::io::Result<()> {
+pub fn write_string_to_file(file_path: &str, contents: &String) -> std::io::Result<()> {
     let folder_path = Path::new(file_path).parent().unwrap();
     fs::create_dir_all(folder_path).expect("Failed to create directory");
     let mut file = fs::File::create(file_path)?;
     file.write_all(contents.as_bytes())?;
+    Ok(())
+}
+
+pub fn create_blank_file_and_overwrite(file_path: &str) -> Result<()> {
+    if Path::new("./out/os/main.json").exists() {
+        fs::remove_file(file_path)?;
+    }
+    fs::File::create(file_path)?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
 mod file;
 mod json;
 mod os_file;
+use std::path::Path;
+use std::{fs, io::Write};
 use walkdir::WalkDir;
 
 fn main() {
     let now = std::time::Instant::now();
     let mut file_count = 0;
     //let mut os_entry_vec = Vec::new();
-    let mut os_entry_key_vec = Vec::new();
+    //let mut os_entry_key_vec = Vec::new();
+
+    fs::create_dir_all("./out/os").expect("Failed to create directory ./out/os");
+    file::create_blank_file_and_overwrite("./out/os/main.json")
+        .expect("Failed to create ./out/os/main.json");
+    let mut main_json_file = fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("./out/os/main.json")
+        .unwrap();
+    main_json_file
+        .write("[".as_bytes())
+        .expect("Failed to write to ./out/os/main.json");
+
+    file::create_blank_file_and_overwrite("./out/os/index.json")
+        .expect("Failed to create ./out/os/index.json");
+    let mut index_json_file = fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("./out/os/index.json")
+        .unwrap();
+    index_json_file
+        .write("[".as_bytes())
+        .expect("Failed to write to ./out/os/index.json");
 
     for entry in WalkDir::new("./appledb/osFiles")
         .into_iter()
@@ -24,25 +51,40 @@ fn main() {
         let entry_vec = os_file::get_os_entry_vec_from_path(path);
 
         for entry in entry_vec {
-            //os_entry_vec.push(entry.clone());
-            os_entry_key_vec.push(entry.clone().key.replace(';', "/").replace(' ', "-"));
-
             let out_json = serde_json::to_string(&entry).expect("Failed to convert struct to JSON");
+
             let output = ["./out/os/", &entry.key.replace(';', "/"), ".json"].concat();
-            file::write_string_to_file(&output, out_json).expect("Failed to write JSON");
+            file::write_string_to_file(&output, &out_json).expect("Failed to write JSON");
+
+            main_json_file
+                .write([out_json.to_string(), ','.to_string()].concat().as_bytes())
+                .expect("Failed to write to ./out/os/main.json");
+            index_json_file
+                .write(
+                    [
+                        entry
+                            .clone()
+                            .key
+                            .replace(';', "/")
+                            .replace(' ', "-")
+                            .to_string(),
+                        ','.to_string(),
+                    ]
+                    .concat()
+                    .as_bytes(),
+                )
+                .expect("Failed to write to ./out/os/index.json");
 
             file_count += 1;
         }
     }
 
-    /*let main_json_string =
-        serde_json::to_string(&os_entry_vec).expect("Failed to convert struct to JSON");
-    file::write_string_to_file("./out/os/main.json", main_json_string)
-    .expect("Failed to write JSON");*/
-    let key_json_string =
-        serde_json::to_string(&os_entry_key_vec).expect("Failed to convert struct to JSON");
-    file::write_string_to_file("./out/os/index.json", key_json_string)
-        .expect("Failed to write JSON");
+    main_json_file
+        .write("]\n".as_bytes())
+        .expect("Failed to write to ./out/os/main.json");
+    index_json_file
+        .write("]\n".as_bytes())
+        .expect("Failed to write to ./out/os/index.json");
 
     let elapsed = now.elapsed();
     println!("Processed {} files in {:.2?}", file_count, elapsed);
