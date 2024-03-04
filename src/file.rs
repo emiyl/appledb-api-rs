07fca9fea911b6c742852046ac18fb855038580a
emiyl <me@emiyl.com>
2024-03-04 14:33:37 +0000

    move os_file stuff to os_file module

--- src/file.rs
+++ src/file.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io::Result;
 use std::io::Write;
 use std::path::Path;
 
@@ -6,7 +7,7 @@ pub fn open_file_to_string(file_path: &str) -> String {
     fs::read_to_string(file_path).expect("Should have been able to read the file") as String
 }
 
-pub fn write_string_to_file(file_path: &str, contents: &String) -> std::io::Result<()> {
+pub fn write_string_to_file(file_path: &str, contents: &String) -> Result<()> {
     let folder_path = Path::new(file_path).parent().unwrap();
     fs::create_dir_all(folder_path).expect("Failed to create directory");
     let mut file = fs::File::create(file_path)?;
@@ -16,4 +17,8 @@ pub fn write_string_to_file(file_path: &str, contents: &String) -> std::io::Resu
 
 pub fn path_exists(path: &String) -> bool {
     Path::new(&path).exists()
+}
+
+pub fn mkdir(path: String) -> Result<()> {
+    fs::create_dir_all(path)
 }
--- src/os_file.rs
+++ src/os_file.rs
@@ -1,10 +1,11 @@
-use crate::file;
-use crate::json;
+use crate::{file, json};
 use serde::Serialize;
 use serde_json::Value;
 use std::collections::BTreeMap;
+use std::{fs, fs::File, io::Write, os::unix::fs::FileExt};
 use struct_field_names_as_array::FieldNamesAsArray;
 use url::Url;
+use walkdir::WalkDir;
 
 structstruck::strike! {
     #[derive(FieldNamesAsArray)]
@@ -301,7 +302,7 @@ pub fn create_os_entry_from_json(json: &Value) -> OsEntry {
     entry
 }
 
-pub fn get_os_entry_vec_from_path(file_path: &str) -> Vec<OsEntry> {
+fn get_os_entry_vec_from_path(file_path: &str) -> Vec<OsEntry> {
     let json_string = file::open_file_to_string(file_path);
     let mut json_vec = vec![json::parse_json(&json_string)];
 
@@ -367,4 +368,130 @@ pub fn get_os_entry_vec_from_path(file_path: &str) -> Vec<OsEntry> {
     }
 
     entry_vec
+}
+
+pub fn create_firmware() -> u32 {
+    let mut file_count: u32 = 0;
+    file::mkdir("./out/firmware".to_string()).expect("Failed to create directory ./out/firmware");
+
+    let main_index_json_path_array = ["./out/firmware/main.json", "./out/firmware/index.json"];
+    let mut main_index_json_file_vec: Vec<File> = Vec::new();
+
+    for path in main_index_json_path_array {
+        file::write_string_to_file(path, &"[".to_string())
+            .expect("Failed to write to main/index json file");
+        main_index_json_file_vec.push(
+            fs::OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(path)
+                .unwrap(),
+        )
+    }
+    let mut os_str_vec: Vec<String> = Vec::new();
+
+    for entry in WalkDir::new("./appledb/osFiles")
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter_map(|entry| {
+            if entry.path().extension().map_or(false, |ext| ext == "json") {
+                Some(entry)
+            } else {
+                None
+            }
+        })
+    {
+        let path = entry.path().to_str().unwrap();
+        let entry_vec = get_os_entry_vec_from_path(path);
+
+        for entry in entry_vec {
+            let out_json = serde_json::to_string(&entry).expect("Failed to convert struct to JSON");
+
+            let output = ["./out/firmware/", &entry.key.replace(';', "/"), ".json"].concat();
+            file::write_string_to_file(&output, &out_json).expect("Failed to write JSON");
+            file_count += 1;
+
+            let os_str = &entry.osStr;
+            let os_str_main_index_json_file_path = [
+                ["./out/firmware/", os_str, "/main.json"].concat(),
+                ["./out/firmware/", os_str, "/index.json"].concat(),
+            ];
+            if !os_str_vec.contains(os_str) {
+                os_str_vec.push(os_str.clone());
+                for path in os_str_main_index_json_file_path.iter() {
+                    if file::path_exists(path) {
+                        fs::remove_file(path).expect("Failed to delete osStr main/index json file")
+                    }
+                }
+            }
+
+            for path in os_str_main_index_json_file_path.iter() {
+                if !file::path_exists(path) {
+                    file::write_string_to_file(path, &"[".to_string())
+                        .expect("Failed to write to osStr main/index json file");
+                }
+            }
+
+            let mut os_str_main_index_json_file_vec: Vec<File> = Vec::new();
+            for path in os_str_main_index_json_file_path.iter() {
+                os_str_main_index_json_file_vec.push(
+                    fs::OpenOptions::new()
+                        .append(true)
+                        .create(true)
+                        .open(path)
+                        .unwrap(),
+                );
+            }
+
+            let main_index_json_file_buf = vec![
+                [out_json, ",".to_string()].concat(),
+                [
+                    "\"".to_string(),
+                    entry.clone().key.replace(';', "/"),
+                    "\",".to_string(),
+                ]
+                .concat(),
+            ];
+
+            for (i, mut file) in os_str_main_index_json_file_vec.iter().enumerate() {
+                file.write_all(main_index_json_file_buf[i].as_bytes())
+                    .expect("Failed to write to osStr main/index json file");
+            }
+            for (i, mut file) in main_index_json_file_vec.iter().enumerate() {
+                file.write_all(main_index_json_file_buf[i].as_bytes())
+                    .expect("Failed to write to main/index json file");
+            }
+        }
+    }
+
+    for os_str in os_str_vec {
+        let os_str_main_index_json_file_path_array = [
+            ["./out/firmware/", os_str.as_str(), "/main.json"].concat(),
+            ["./out/firmware/", os_str.as_str(), "/index.json"].concat(),
+        ];
+
+        for path in os_str_main_index_json_file_path_array {
+            let file = fs::OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(path)
+                .unwrap();
+            let len = file.metadata().unwrap().len();
+            let pos = if len > 1 { len - 1 } else { len };
+            file.write_at("]\n".as_bytes(), pos)
+                .expect("Failed to write to osStr main.json");
+        }
+
+        file_count += 1;
+    }
+
+    for file in main_index_json_file_vec {
+        let len = file.metadata().unwrap().len();
+        let pos = if len > 1 { len - 1 } else { len };
+        file.write_at("]\n".as_bytes(), pos)
+            .expect("Failed to write to main/index json file");
+        file_count += 1;
+    }
+
+    file_count
 }
